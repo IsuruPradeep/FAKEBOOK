3b6f60677e17e5b4a344af49a2e1cbe176420eae
IsuruPradeep <pisuru136@gmail.com>
2025-09-03 01:56:31 +0530

    End nav bar

--- frontend/src/components/Navigation/NavigBar.js
+++ frontend/src/components/Navigation/NavigBar.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./navbar.css";
 import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import SettingsIcon from '@mui/icons-material/Settings';
+
 
 export default function NavigBar() {
   return (
@@ -15,12 +19,26 @@ export default function NavigBar() {
         </div>
       </div>
       <div class="navBarRight">
-        <div className="navBarLink">
+        <div className="navBarLinks">
           <span className="navBarLink">HomePage</span>
           <span className="navBarLink">Profile</span>
         </div>
         <div className="navBarIcons">
-          <div className="navBarIcon"></div>
+          <div className="navBarIcon">
+            <PersonIcon/>
+            <span class="iconTag">3</span>
+          </div>
+          <div className="navBarIcon">
+            <ChatIcon/>
+            <span class="iconTag">5</span>
+          </div>
+          <div className="navBarIcon">
+            <SettingsIcon/>
+            <span class="iconTag">2</span>
+          </div>
+        </div>
+        <div class="pic">
+          <img src="/images/img5.jpg" alt="" class="profilePicImg"/>
         </div>
       </div>
     </div>
