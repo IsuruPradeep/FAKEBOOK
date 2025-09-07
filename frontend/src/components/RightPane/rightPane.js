b832096cccacbe27b0efb16c24590bdd0f41dbfa
IsuruPradeep <pisuru136@gmail.com>
2025-09-08 02:14:24 +0530

    end of frontend

--- frontend/src/components/RightPane/rightPane.js
+++ frontend/src/components/RightPane/rightPane.js
@@ -8,6 +8,70 @@ export default function rightPane() {
         <div class="adcontainer">
           <span class="sponsered">Sponsored</span>
           <img src="/images/add.jpeg" alt="" class="addImage"/>
+          <span class="addText">
+            Order Your food From Us..!
+          </span>
+        </div>
+        <div class="birthDayContainer">
+          <img src="/images/gift.png" alt="" class="birthdayImage"/>
+          <span class="birthdaytext"><b>Kasun Gayantha</b> and <b> 2 others </b>having Birthday today...!</span>
+        </div>
+        <div class="contact">
+          <h3 className='contact'>Contact</h3>
+        <div class="friendList">
+          <li class="onlineFrined">
+            <div class="onlineFrinedImageContainer">
+              <img src="/images/person.jpg" alt="" class="onlineFriendImg"/>
+              <span class="onlineStatus"></span>
+            </div>
+            <span class="onlineFriendName">Bald Jonny Siriwardhana</span>
+          </li>
+        </div>
+        <div class="friendList">
+          <li class="onlineFrined">
+            <div class="onlineFrinedImageContainer">
+              <img src="/images/person.jpg" alt="" class="onlineFriendImg"/>
+              <span class="onlineStatus"></span>
+            </div>
+            <span class="onlineFriendName">Bald Jonny Siriwardhana</span>
+          </li>
+        </div>
+        <div class="friendList">
+          <li class="onlineFrined">
+            <div class="onlineFrinedImageContainer">
+              <img src="/images/person.jpg" alt="" class="onlineFriendImg"/>
+              <span class="onlineStatus"></span>
+            </div>
+            <span class="onlineFriendName">Bald Jonny Siriwardhana</span>
+          </li>
+        </div>
+        <div class="friendList">
+          <li class="onlineFrined">
+            <div class="onlineFrinedImageContainer">
+              <img src="/images/person.jpg" alt="" class="onlineFriendImg"/>
+              <span class="onlineStatus"></span>
+            </div>
+            <span class="onlineFriendName">Bald Jonny Siriwardhana</span>
+          </li>
+        </div>
+        <div class="friendList">
+          <li class="onlineFrined">
+            <div class="onlineFrinedImageContainer">
+              <img src="/images/person.jpg" alt="" class="onlineFriendImg"/>
+              <span class="onlineStatus"></span>
+            </div>
+            <span class="onlineFriendName">Bald Jonny Siriwardhana</span>
+          </li>
+        </div>
+        <div class="friendList">
+          <li class="onlineFrined">
+            <div class="onlineFrinedImageContainer">
+              <img src="/images/person.jpg" alt="" class="onlineFriendImg"/>
+              <span class="onlineStatus"></span>
+            </div>
+            <span class="onlineFriendName">Bald Jonny Siriwardhana</span>
+          </li>
+        </div>
         </div>
       </div>
     </div>
